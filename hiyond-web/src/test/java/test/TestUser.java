87b25397b112b08faf55da954fbc766a01314267
michealHiyond <zhy984293834@hotmail.com>
2016-05-27 18:05:03 +0800

    ..

--- hiyond-web/src/test/java/test/TestUser.java
+++ hiyond-web/src/test/java/test/TestUser.java
@@ -1,5 +1,9 @@
 package test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.junit.Test;
 
 import entity.User;
@@ -12,6 +16,20 @@ public class TestUser {
 		user.setId(1);
 		user.setName("hiyond");
 		System.out.println(user);
+	}
+
+	@Test
+	public void test02() {
+		try {
+			Properties properties = new Properties();
+			InputStream inputStream = TestUser.class.getResourceAsStream("/redis/redis.properties");
+			properties.load(inputStream);
+			System.out.println(properties.toString());
+			inputStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
