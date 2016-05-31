26ffedcbd64c1340bd27f8be7f584f3bfd91e213
michealHiyond <zhy984293834@hotmail.com>
2016-05-31 17:30:56 +0800

    ..

---
+++ hiyond-common/src/test/java/com/hiyond/common/thread/ThreadMain.java
@@ -0,0 +1,5 @@
+package com.hiyond.common.thread;
+
+public class ThreadMain {
+
+}
---
+++ hiyond-common/src/test/java/com/hiyond/common/thread/ThreadTest.java
@@ -0,0 +1,17 @@
+package com.hiyond.common.thread;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * 
+ * @author hiyond
+ *
+ */
+public class ThreadTest implements Runnable {
+
+	@Override
+	public void run() {
+		
+	}
+
+}
