fd832b393938bea622169b547ddce8e76d1397a3
michealHiyond <zhy984293834@hotmail.com>
2016-09-06 18:20:23 +0800

    ..

--- hiyond-lucene/src/main/java/com/hiyond/lucene/core/LuceneTest.java
+++ hiyond-lucene/src/main/java/com/hiyond/lucene/core/LuceneTest.java
@@ -1,7 +1,57 @@
 package com.hiyond.lucene.core;
 
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.*;
+import org.apache.lucene.store.RAMDirectory;
+
+import java.io.IOException;
+
 /**
  * Created by hiyond on 2016/9/6.
  */
 public class LuceneTest {
+
+    public static void main(String[] args) throws IOException {
+        RAMDirectory directory = new RAMDirectory();
+
+
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+        IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
+
+        IndexWriter indexWriter = new IndexWriter(directory, indexWriterConfig);
+
+        Document doc = new Document();
+
+        doc.add(new Field("name", "lin zhengle", TextField.TYPE_STORED));
+        doc.add(new Field("address", "中国上海", TextField.TYPE_STORED));
+        doc.add(new Field("dosometing", "I am learning lucene ",TextField.TYPE_STORED));
+        doc.add(new Field("dosometing", "learning lucene hehe hehe",TextField.TYPE_STORED));
+
+        indexWriter.addDocument(doc);
+        indexWriter.close();
+
+        DirectoryReader directoryReader = DirectoryReader.open(directory);
+        IndexSearcher indexSearcher = new IndexSearcher(directoryReader);
+        Term term = new Term("dosometing", "lucene");
+        Query query = new TermQuery(term);
+
+
+        TopDocs topDocs = indexSearcher.search(query,100);
+        System.out.println(topDocs.totalHits);
+
+        for (ScoreDoc scoreDoc : topDocs.scoreDocs){
+            Document document = indexSearcher.doc(scoreDoc.doc);
+            System.out.println(document.toString());
+        }
+
+        indexWriter.close();
+        directory.close();
+    }
 }
---
+++ hiyond-lucene/src/main/java/com/hiyond/lucene/core/util/LuceneUtils.java
@@ -0,0 +1,41 @@
+package com.hiyond.lucene.core.util;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+/**
+ * Created by hiyond on 2016/9/6.
+ */
+public class LuceneUtils {
+
+    public static IndexWriter getIndexWriter(String indexPath, boolean create) throws IOException {
+        //创建目录对象
+        Directory directory = FSDirectory.open(Paths.get(indexPath, new String[0]));
+
+        //创建分析器
+        Analyzer analyzer = new StandardAnalyzer();
+
+        //IndexWriterConfig对象创建,
+        IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
+        if(create){
+            //如果是创建索引
+            indexWriterConfig.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+        }else {
+            //追加索引
+            indexWriterConfig.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+        }
+
+        IndexWriter indexWriter = new IndexWriter(directory, indexWriterConfig);
+
+        return indexWriter;
+    }
+
+
+}
