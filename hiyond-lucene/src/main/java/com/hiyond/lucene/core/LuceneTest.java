986f1558bc6770367a0d5074ee0dc795ece5b477
michealHiyond <zhy984293834@hotmail.com>
2016-09-07 13:45:57 +0800

    lucene

--- hiyond-lucene/src/main/java/com/hiyond/lucene/core/LuceneTest.java
+++ hiyond-lucene/src/main/java/com/hiyond/lucene/core/LuceneTest.java
@@ -1,5 +1,6 @@
 package com.hiyond.lucene.core;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -8,6 +9,8 @@ import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.RAMDirectory;
 
@@ -18,7 +21,7 @@ import java.io.IOException;
  */
 public class LuceneTest {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         RAMDirectory directory = new RAMDirectory();
 
 
@@ -30,10 +33,18 @@ public class LuceneTest {
         Document doc = new Document();
 
         doc.add(new Field("name", "lin zhengle", TextField.TYPE_STORED));
-        doc.add(new Field("address", "中国上海", TextField.TYPE_STORED));
-        doc.add(new Field("dosometing", "I am learning lucene ",TextField.TYPE_STORED));
-        doc.add(new Field("dosometing", "learning lucene hehe hehe",TextField.TYPE_STORED));
+        indexWriter.addDocument(doc);
 
+        doc = new Document();
+        doc.add(new Field("address", "中国上海", TextField.TYPE_STORED));
+        indexWriter.addDocument(doc);
+
+        doc = new Document();
+        doc.add(new Field("dosometing", "I am learning lucene ",TextField.TYPE_STORED));
+        indexWriter.addDocument(doc);
+
+        doc = new Document();
+        doc.add(new Field("dosometing", "learning lucene hehe hehe", TextField.TYPE_STORED));
         indexWriter.addDocument(doc);
         indexWriter.close();
 
@@ -41,15 +52,15 @@ public class LuceneTest {
         IndexSearcher indexSearcher = new IndexSearcher(directoryReader);
         Term term = new Term("dosometing", "lucene");
         Query query = new TermQuery(term);
-
-
-        TopDocs topDocs = indexSearcher.search(query,100);
+//        QueryParser queryParser = new QueryParser("dosometing",analyzer);
+//        query = queryParser.parse("lucene");
+        TopDocs topDocs = indexSearcher.search(query,2);
         System.out.println(topDocs.totalHits);
-
         for (ScoreDoc scoreDoc : topDocs.scoreDocs){
             Document document = indexSearcher.doc(scoreDoc.doc);
-            System.out.println(document.toString());
+            System.out.println(document.get("dosometing"));
         }
+
 
         indexWriter.close();
         directory.close();
