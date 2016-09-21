e2675df00547af83a17a1fdf969f4ba9d3cfdf5c
michealHiyond <zhy984293834@hotmail.com>
2016-09-21 18:20:15 +0800

    ..

--- hiyond-common/src/main/java/com/hiyond/common/mongodb/MongoUtils.java
+++ hiyond-common/src/main/java/com/hiyond/common/mongodb/MongoUtils.java
@@ -1,7 +1,27 @@
 package com.hiyond.common.mongodb;
 
+import com.mongodb.client.MongoDatabase;
+import org.apache.log4j.Logger;
+import org.bson.Document;
+
 /**
  * Created by hiyond on 2016/9/21.
  */
 public class MongoUtils {
+
+    private static Logger logger = Logger.getLogger(MongoUtils.class);
+
+    private static final boolean SUCCESS = true;
+    private static final boolean FAILD = false;
+
+    public static boolean insertCollection(Document document, String collectionName) {
+        try {
+            MongoDatabase database = MongoCenter.getMongoDatabase();
+            database.getCollection(collectionName).insertOne(document);
+            return SUCCESS;
+        }catch (Exception e){
+            logger.error("插入失败，参数{},{},{}"+document+collectionName,e);
+            return FAILD;
+        }
+    }
 }
